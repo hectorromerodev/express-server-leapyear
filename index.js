dfa2839a94ad56eb2ae6d2859a7076438d815f73
hectorromerodev <hectorromero.developer@gmail.com>
2021-01-05 14:48:07 -0800

    UPD index.js logic

--- index.js
+++ index.js
@@ -4,15 +4,22 @@ const app = express()
 const { config } = require('./config/index')
 
 app.get('/', (req, res) => {
-  res.json({ res });
+  res.send("This is a get with '/' ")
 });
 
-app.get('/leapyear', (req, res) => {
+app.get('/leapyear/:year', (req, res) => {
+  const year = req.params.year;
   res.json({
-    year: 'asd',
+    isLeapYear: isLeapYear(year),
   });
-});
+}); 
 
 app.listen(config.port, () => {
   console.log(`Listening http://localhost:${config.port}`)
 });
+
+const isLeapYear = (year) => {
+  return (year % 400 === 0) ? true :
+    (year % 100 === 0) ? false :
+      year % 4 === 0;
+};
